c8ed725bd12706cb9dd93d513c72cc5deeb45d5c
Nardos Tilahun <nardostilahun83@gmail.com>
2023-09-15 15:17:43 +0300

    Feature Update: Updated page design and changed project details

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { AddTask } from "./components/AddTask";
+import "./App.css";
+import AddTask from "./components/AddTask";
+import Filter from "./components/Filter";
+import Tasks from "./components/Tasks";
 
-// import TaskItem from './components/'
 function App() {
   return (
-    <div className="text-white">
-      <div>
-        <h1> Task Manager.</h1>
-      </div>
-      <div>
-        <AddTask />
-      </div>
-    </div>
+    <main className=" bg-gray-100 dark:bg-slate-900 h-screen">
+      <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+        Task Manager.
+      </h1>
+      <AddTask />
+      <Filter />
+      <Tasks />
+    </main>
   );
 }
-
 export default App;
--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useTasks } from "../states/taskContext";
+import { useTasks } from "../store/TaskContext";
 
 const AddTask = () => {
   const { handleAddTask } = useTasks();
